86c9cc503a1f674638e3fef4fd30b8494c7c3935
Iain Brown <codeiain@outlook.com>
2022-05-01 13:22:33 +0000

    blaa

--- cop/src/app/interceptor.service.ts
+++ cop/src/app/interceptor.service.ts
@@ -1,31 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpInterceptor, HttpResponse } from '@angular/common/http';
 import { HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable, retry } from 'rxjs';
 import { HttpHandler } from '@angular/common/http';
 import { HttpEvent } from '@angular/common/http';
 import { Guid } from 'guid-typescript';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
   token: string | undefined;
-  omitCalls = ['auth'];
+  omitCalls = ['auth']; //part of url or full url
   skipInterceptor = false;
-  constructor() { }
+  constructor(private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+    this.omitCalls.forEach(api => {
+      if (req.url.includes(api)) {
+        this.skipInterceptor = true;
+      }
+    });
     this.token = 'TokenHere'
-    if (this.token) {
-      const tokenizedReq = req.clone(
-        { headers: req.headers.set('Authorization', 'Bearer ' + this.token)
-        .set('CorrelationId',Guid.create().toString())
-       });
-      return next.handle(tokenizedReq);
+    if (this.token || this.skipInterceptor) {
+      const tokenizedReq = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + this.token).set('CorrelationId',Guid.create().toString()) });
+      return next.handle(tokenizedReq).pipe(map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          if (event.status === 401) {
+            //navigate back to login page
+            this.router.navigateByUrl('core/login');
+          }
+        }
+        return event;
+      }));
+    } else {
+      this.router.navigateByUrl('core/login');
     }
-    return next.handle(req);
+    return next.handle(req).pipe(retry(3));
   }
 
   
